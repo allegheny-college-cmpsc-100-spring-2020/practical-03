6717b006871f0b8024f3483ff2f55a9565af862b
Douglas Luman <dluman@douglasjluman.com>
2020-02-07 10:16:18 -0500

    Initial commit

---
+++ src/main/java/practicalthree/GatorTemp.java
@@ -0,0 +1,26 @@
+package practicalthree;
+
+/** Implements a temperature check for dormant gators.
+ *
+ * @author Solution Repository
+ */
+public class GatorTemp {
+
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+       // Print result
+     // Create "truthy" test
+public static void main(String[] args) {
+     boolean isTemperate >= goodtemp
+   }
+     System.out.println("Temperature good for gators: " + isTemperate);
+     // Comfortable gator temperature
+     // Evaluate conversion expression
+     System.out.println("Changing temperature from + tempInF " + F -> + " + tempInC + C");
+     double tempInC = 5 / 9 * tempInF - 32;
+     int tempInF = 55;
+     int goodTemp = 10.5;
+     // High temperature in celsius
+}
